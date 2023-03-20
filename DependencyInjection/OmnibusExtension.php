e48c5ba8f13b4cfafafe08199950c75680dca9db
Marco Meyer <marco.meyer@glitchr.io>
2023-03-20 23:36:07 +0900

    update omnibus bundle structure for symfony

---
+++ DependencyInjection/OmnibusExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Omnibus\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\Definition\Processor;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class OmnibusExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // Format XML
+        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__, 1).'/Resources/config'));
+        $loader->load('services.xml');
+
+        $processor = new Processor();
+        $configuration = new Configuration();
+        $config = $processor->processConfiguration($configuration, $configs);
+        $this->setConfiguration($container, $config, $configuration->getTreeBuilder()->getRootNode()->getNode()->getName());
+    }
+
+    public function setConfiguration(ContainerBuilder $container, array $config, $globalKey = "")
+    {
+        foreach ($config as $key => $value) {
+            if (!empty($globalKey)) {
+                $key = $globalKey.".".$key;
+            }
+
+            if (is_array($value)) {
+                $this->setConfiguration($container, $value, $key);
+            } else {
+                $container->setParameter($key, $value);
+            }
+        }
+    }
+}
---
+++ OmnibusBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Omnibus\;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OmnibusBundle extends Bundle
+{
+}
